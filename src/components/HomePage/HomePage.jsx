d5eb61efed60f2371418e5df7a52a03a8168128c
LetuQingge <qingge1231@gmail.com>
2023-08-28 15:12:07 -0400

    Update HomePage.jsx

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -80,7 +80,7 @@ const teachings = [
   "COMP 385 Theory of Computing, Fall 2022",
   "COMP 496 Senior Project II, Fall 2022",
   "COMP 841 Deep Learning, Spring 2022",
-  "COMP 841 Deep Learning, Spring 2022",
+  "COMP 496 Senior Project II, Spring 2022",
   "COMP 285 Analysis of Algorithms, Fall 2021",
   "COMP 496 Senior Project II, Fall 2021",
   "COMP 841 Deep Learning, Spring 2021",
